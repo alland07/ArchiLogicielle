1c775186a983a740bca932558c2ce25bce089e4d
Julie Souchet <90184650+SouchetJulie@users.noreply.github.com>
2022-12-12 13:59:28 +0100

    Format

--- tdd/decomposition.ts
+++ tdd/decomposition.ts
@@ -5,6 +5,7 @@ export const Decomposition = (nombre : number): Array<Number> => {
   const facteurs: number[] = [];
   let reste = nombre;
   let diviseur = 2; // on commence par 2, car 1 est traité au-dessus
+
   while (reste > 1) { // tant qu'il reste des facteurs à trouver
     if (reste % diviseur === 0) { // diviseur est un facteur → on l'ajoute
       facteurs.push(diviseur);
